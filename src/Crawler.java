162d093b36ee77b7a507274a16560105678f4163
Amanda Chang <43429828+amandajyu@users.noreply.github.com>
2021-09-28 17:13:50 -0700

    Updated Crawler.java
    Crawler will now check for language before saving the html and plain text files of the webpage.
    Added many comments to aid in understanding the code

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,51 +1,68 @@
+import org.apache.tika.language.LanguageIdentifier;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashSet;
+
 /*TODO:
-check the page for language before downloading
-save the html to repository folder
 add the URL and number of outlinks to report.csv
 */
 
 public class Crawler {
-
+    //hashset stores unique values
     private HashSet<String> links;
+    //count keeps track of the number of URLS crawled
     private int count =0;
+    //max is the maximum number of crawls we want to do
+    private int max =1;
+    //what language do we want the webpages to be in
+    private String desiredLanguage = "en";
 
+    //each Crawler gets its own hashset
     public Crawler() {
         links = new HashSet<String>();
     }
 
     public void getLinks(String URL) {
-        String html;
-        //checks if you have already crawled the URLs but doesn't check for duplicate content
+        //check if the url has already been crawled
         if (!links.contains(URL)) {
             try {
                 //If not add it to the index
                 if (links.add(URL)) {
                     //print the URL
                     System.out.println(URL);
-                    html = Jsoup.connect(URL).get().html();
-                    //print the html code
-                    System.out.println(html);
-                    //only crawl up to the number count
-                    count +=1;
-                    if (count>=2){
-                        System.exit(0);
-                    }
                 }
-                //gets the HTML
+                //gets the HTML?
                 Document document = Jsoup.connect(URL).get();
                 //extract links to other URLs from the HTML
                 Elements linksOnPage = document.select("a[href]");
-                //For each extracted URL go back to start of try
+                //check what language the webpage is
+                LanguageIdentifier identifier = new LanguageIdentifier(document.body().text());
+                String language = identifier.getLanguage();
+                //only save the html and text if webpage is in desired language
+                if(language.equals(desiredLanguage)){
+                    count +=1;
+                    //save the html of the webpage to repository folder
+                    PrintWriter out = new PrintWriter("./repository/" + count + ".html");
+                    out.println(document);
+                    out.close();
+                    //save the plain text of the webpage to the text fodler
+                    PrintWriter out2 = new PrintWriter("./text/" + count + ".txt");
+                    out2.println(document.text());
+                    out2.close();
+                    //only crawl up to the number count
+                    if (count>=max){
+                        System.exit(0);
+                    }
+                }
+                //For each extracted URL perform the getLinks method
                 for (Element page : linksOnPage) {
                     getLinks(page.attr("abs:href"));
                 }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 System.err.println("For '" + URL + "': " + e.getMessage());
             }
         }
@@ -53,7 +70,7 @@ public class Crawler {
 
     public static void main(String[] args) {
         //enter a URL as the seed to start the crawl
-        new Crawler().getLinks("https://en.wikipedia.org/");
+        new Crawler().getLinks("https://en.wikipedia.org/wiki/California_State_Polytechnic_University,_Pomona");
     }
 
 }
